9d164fbe543ca7cf18d6bdcb30b9f869985b4808
Igor Moura <142421648+IgorMoura1@users.noreply.github.com>
2025-08-18 21:23:35 -0300

    feat/ route for get all products done

--- cmd/main.go
+++ cmd/main.go
@@ -3,13 +3,28 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"goapi/controller"
+	"goapi/db"
+	"goapi/repository"
+	"goapi/usecase"
 )
 
 func main() {
 
 	server := gin.Default()
 
-	ProductController := controller.NewProductController()
+	dbConnection, err := db.ConnectDB()
+	if err != nil {
+		panic(err)
+	}
+
+	//Camada de repositories
+	ProductRepository := repository.NewProductRepository(dbConnection)
+
+	//Camada de usecases
+	ProductUseCase := usecase.NewProductUseCase(ProductRepository)
+
+	// Camada de controllers
+	ProductController := controller.NewProductController(ProductUseCase)
 	//server.GET("/products", ProductController.GetProducts)
 
 	server.GET("/ping", func(ctx *gin.Context) {
